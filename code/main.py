b6b1623cc0711edb039e92e211b52c09f73cffd5
kristian-girsang <kristian.girsang@ui.ac.id>
2023-01-23 19:28:16 +0700

    first commit

---
+++ code/main.py
@@ -0,0 +1,33 @@
+import pygame, sys
+from settings import *
+
+class Game:
+    def __init__(self):
+        # General setup
+        # Initialize the pygame library
+        pygame.init()
+        # Set the screen size
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Zelda')
+        # Create a clock object to control the game's frame rate
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        # Start the game loop
+        while True:
+            # Checkfor QUIT event (e.g. when the window is closed)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            # Clear the screen to black
+            self.screen.fill('black')
+            # Update the display
+            pygame.display.update()
+            # Limit the frame rate
+            self.clock.tick(FPS)
+
+# Create and instance of the game and start the game loop
+if __name__ == '__main__':
+    game = Game()
+    game.run()
